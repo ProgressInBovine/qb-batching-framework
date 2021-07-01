830627c91bb8a3748bd1cd336b60db54f05a8ab1
ProgressInBovine <qingbinbqt@163.com>
2021-07-01 14:27:47 +0800

    【框架】数据分片1

---
+++ src/main/java/data/split/DataSplit.java
@@ -0,0 +1,11 @@
+package data.split;
+
+/**
+ * @PackgeName: data.split
+ * @Author: Creed of QB
+ * @Date: 2021/7/1 14:26
+ * @project: qb-batching-framework
+ * @Description:
+ */
+public class DataSplit {
+}
---
+++ src/main/java/data/split/StreamTools.java
@@ -0,0 +1,11 @@
+package data.split;
+
+/**
+ * @PackgeName: data.split
+ * @Author: Creed of QB
+ * @Date: 2021/7/1 14:27
+ * @project: qb-batching-framework
+ * @Description:
+ */
+public class StreamTools {
+}
